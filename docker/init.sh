0308fcc295f10d015e46ca83f3f5e1fa4a74deee
atsglobalinfo <info@atsglobal.in>
2026-07-03 11:45:16 +0530

    fix: don't fail on unset NVM_DIR/NODE_VERSION_DEVELOP under set -u

--- docker/init.sh
+++ docker/init.sh
@@ -16,7 +16,7 @@ else
     echo "Creating new bench..."
 fi
 
-export PATH="${NVM_DIR}/versions/node/v${NODE_VERSION_DEVELOP}/bin/:${PATH}"
+export PATH="${NVM_DIR:-}/versions/node/v${NODE_VERSION_DEVELOP:-}/bin/:${PATH}"
 
 bench init --skip-redis-config-generation frappe-bench
 
